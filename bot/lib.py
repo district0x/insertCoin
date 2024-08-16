d5c747eada1f31963153e6c9e0a04dee5d785d61
Bhavesh <bhavesh@jklu.edu.in>
2024-08-17 01:24:15 +0530

    improved funtionalities in bot

--- bot/lib.py
+++ bot/lib.py
@@ -4,8 +4,8 @@ from web3 import Web3
 from supabase import create_client, Client
 
 def initialize_supabase():
-    SUPABASE_URL = 'https://bqrkkxsfkpxyxflsqtlo.supabase.co'
-    SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxcmtreHNma3B4eXhmbHNxdGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwMTI4MDUsImV4cCI6MjAzMjU4ODgwNX0.cK5UruJIJttzBYi5qSttFKCbETtkpvv-oj-EVJM1vKU'
+    SUPABASE_URL = 'https://lyjimsetpystcpprjxac.supabase.co'
+    SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5amltc2V0cHlzdGNwcHJqeGFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MjM1OTMsImV4cCI6MjAzNTQ5OTU5M30.Bk4sroHIT5tmaPO9r4fRKBv5ZrvyOxBRn2xIgsuFA28'
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
     return supabase
 
@@ -15,7 +15,7 @@ def get_next_match_id():
 
     # Load contract ABI from JSON file
     with open('contractABI.json', 'r') as abi_file:
-        contract_abi = json.load(abi_file)
+        contract_abi = json.load(abi_file) 
 
     # Contract address
     contract_address = '0x8d7b8A8ba6f29615810FAd89Fe2F34c359784De9'
